e9c0d74d70fda55807213ab6885e666efef7c6e1
Ivan Zhevakin <ixvakin@gmail.com>
2018-08-20 17:35:12 +0300

    drawer redesign

--- src/navigation/CustomDrawer.js
+++ src/navigation/CustomDrawer.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Button, ScrollView, StyleSheet } from 'react-native'
+import PropTypes from 'prop-types'
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-navigation'
 
-export default class CustomDrawer extends React.Component {
-
+class CustomDrawer extends React.Component {
   handleChangeGame = () => {
     this.props.navigation.navigate('Game')
   }
@@ -16,14 +16,24 @@ export default class CustomDrawer extends React.Component {
     return (
       <ScrollView>
         <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
-          <Button
-            title="Войти в другую игру"
-            onPress={this.handleChangeGame}
-          />
-          <Button
-            title="Сменить команду"
-            onPress={this.handleLogout}
-          />
+          <View style={styles.logo}>
+            <Image
+              source={require('../../assets/icon.png')}
+              style={styles.logoImg}
+            />
+          </View>
+          <View style={styles.menu}>
+            <TouchableOpacity onPress={this.handleChangeGame}>
+              <View style={styles.menuItem}>
+                <Text style={styles.menuItemText}>ВОЙТИ В ДРУГУЮ ИГРУ</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.handleLogout}>
+              <View style={styles.menuItem}>
+                <Text style={styles.menuItemText}>СМЕНИТЬ КОМАНДУ</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </SafeAreaView>
       </ScrollView>
     )
@@ -32,7 +42,34 @@ export default class CustomDrawer extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 100,
+    paddingTop: 20,
+  },
+  logo: {
+    alignItems: 'center',
+  },
+  logoImg: {
+    height: 130,
+    marginBottom: 20,
+    width: 130,
+  },
+  menu: {
+    borderTopColor: '#ddd',
+    borderTopWidth: 1,
+  },
+  menuItem: {
+    borderBottomColor: '#ddd',
+    borderBottomWidth: 1,
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
+  menuItemText: {
+    fontSize: 16,
+    textAlign: 'center',
   },
 })
+
+CustomDrawer.propTypes = {
+  navigation: PropTypes.object,
+}
+
+export default CustomDrawer
